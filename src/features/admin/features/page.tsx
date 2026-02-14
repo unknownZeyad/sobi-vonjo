50d7d88ac81296a9c3dc2d37a2b9585c9bbc8dad
Zeyad Tamer <160682046+mayBeZoz@users.noreply.github.com>
2026-02-14 09:09:58 +0200

    fix-

--- src/features/admin/features/page.tsx
+++ src/features/admin/features/page.tsx
@@ -36,30 +36,18 @@ export default function Admin() {
   console.log(phase)
   return (
     <div className="w-full h-screen bg-black relative">
-      <AnimatePresence mode="wait">
-        {(() => {
-          switch (phase) {
-            case "start_experience":
-              return <StartExperince key="start_experience" />;
-            case "intro":
-              return <Welcome key="intro" />;
-            case "speed_intro":
-              return <SpeedIntro key="speed_intro" />;
-            case "speed_question":
-              return <SpeedQuestion key="speed_question" />;
-            case "choosing_clubs":
-              return <ChoosingClub key="choosing_clubs" />;
-            case "main_questions":
-              return <AdminMainQuestions key="main_questions" />; // Static key!
-            case "winner":
-              return <WinnerScreen key="winner" />;
-            case "draw":
-              return <Draw key="draw" />;
-            default:
-              return null;
-          }
-        })()}
-      </AnimatePresence>
+      {phase === "start_experience" && (
+        <StartExperince key="start_experience" />
+      )}
+      {phase === "intro" && <Welcome key="welcome" />}
+      {phase === "speed_intro" && <SpeedIntro key="speed_intro" />}
+      {phase === "speed_question" && <SpeedQuestion key="speed_question" />}
+      {phase === "choosing_clubs" && (
+        <ChoosingClub key="choosing_clubs" />
+      )}
+      {phase === "main_questions" && <AdminMainQuestions key="main_questions" />}
+      {phase === "winner" && <WinnerScreen key="winner" />}
+      {phase === "draw" && <Draw key="draw" />}
 
       <motion.button
         onClick={handleReset}
